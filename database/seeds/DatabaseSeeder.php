bb4cb5e6169552597cd9b9af81885bcf6970f7a4
le thu <minhthu512586@gmail.com>
2021-11-09 01:22:09 +0700

    update database

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::unprepared(file_get_contents('database/seeds/sell_cake_db'));
+    //        $this->call(UsersTableSeeder::class);
+    }
+}
